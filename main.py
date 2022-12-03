924f7dd3a52c6262ce57a16e1763346a14c3e474
trevor <trevortds@gmail.com>
2022-12-03 15:05:48 -0600

    switched to scheduler

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # This is a sample Python script.
 import time
+import schedule
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -29,7 +30,7 @@ b6workspace = client.workspaces.find_by_id(1203279608416517)
 decisions_project = client.projects.find_by_id(1203279731709143)
 chores_project = client.projects.find_by_id(1203288705129613)
 known_projects = [decisions_project, chores_project]
-known_project_ids = map( lambda x: x["gid"], known_projects)
+known_project_ids = list(map( lambda x: x["gid"], known_projects))
 
 for proj in list(client.projects.find_all(workspace=b6workspace["gid"])):
     if proj["gid"] not in known_project_ids:
@@ -44,6 +45,10 @@ def get_events_sync_token(resource_id):
     except InvalidTokenError as e:
         return e.sync
 
+print(known_project_ids)
+sync_tokens = {resource_id: get_events_sync_token(resource_id) for resource_id in known_project_ids}
+print(sync_tokens)
+
 
 def fetch_events_for_one_resource(resource_id, sync=None):
     try:
@@ -55,17 +60,13 @@ def fetch_events_for_one_resource(resource_id, sync=None):
         initial_sync = e.sync
         return fetch_events_for_one_resource(resource_id, e.sync)
 
+
 def get_events_from_resources(resource_ids):
-    sync_tokens = {resource_id: get_events_sync_token(resource_id) for resource_id in resource_ids}
-
-    data = None
-    while True:
-        time.sleep(10)
-        for resource_id in resource_ids:
-            data = fetch_events_for_one_resource(resource_id, sync_tokens[resource_id])
-            sync_tokens[resource_id] = data["sync"]
-            yield from data["data"]
-
+    print(sync_tokens)
+    for resource_id in resource_ids:
+        data = fetch_events_for_one_resource(resource_id, sync_tokens[resource_id])
+        sync_tokens[resource_id] = data["sync"]
+        yield from data["data"]
 
 
 def on_new_tasks(event, callbacks: Iterable[Callable] = []):
@@ -103,21 +104,33 @@ def print_new(event):
 
 def process_events():
     event_generator = get_events_from_resources([chores_project["gid"], decisions_project["gid"]])
-    while True:
-        next_event = next(event_generator)
+
+    for next_event in event_generator:
         on_new_tasks(next_event, [print_new])
         on_new_assignee(next_event, [print_assignee])
 
 
+def schedule_test(text):
+    print("this text was scheduled for now: " + text)
+
+def main():
+
+    schedule.every(10).seconds.do(process_events)
+    schedule.every().saturday.at("15:00").do(schedule_test, text="it's three pm")
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     while True:
-        try:
-            process_events()
-        except Exception as e:
-            print("Crashed! restarting...")
-            print(e)
+        main()
+        # try:
+        #     main()
+        # except Exception as e:
+        #     print("Crashed! restarting...")
+        #     print(e)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
