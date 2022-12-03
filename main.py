dccf9d9875549b4112c33b8511de5a629b76f981
Trevor Sullivan <trevor@trevor-sullivan.tech>
2022-12-03 11:03:16 -0600

    init

---
+++ main.py
@@ -0,0 +1,34 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import asana
+
+# Note: Replace this value with your own personal access token
+
+personal_access_token = "censored"
+
+# Construct an Asana client
+client = asana.Client.access_token(personal_access_token)
+
+# Set things up to send the name of this script to us to show that you succeeded! This is optional.
+client.options['client_name'] = "hello_world_python"
+
+# Get your user info
+me = client.users.me()
+
+# Print out your information
+print("Hello world! " + "My name is " + me['name'] + "!")
+
+client.projects.find_all
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
